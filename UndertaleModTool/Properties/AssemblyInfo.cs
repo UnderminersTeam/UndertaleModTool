c9af2316ce95dfbcdb8172f67994ec64d9aa1c23
krzys-h <krzys_h@interia.pl>
2018-11-12 00:13:32 +0100

    Bump version numbers

--- UndertaleModTool/Properties/AssemblyInfo.cs
+++ UndertaleModTool/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.16")]
-[assembly: AssemblyFileVersion("0.1.16")]
+[assembly: AssemblyVersion("0.1.17")]
+[assembly: AssemblyFileVersion("0.1.17")]
