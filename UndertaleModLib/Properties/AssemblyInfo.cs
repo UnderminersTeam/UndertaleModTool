831d61e1c5b8429339f6fa76af122373bf492714
krzys-h <krzys_h@interia.pl>
2018-11-19 22:19:11 +0100

    Bump version numbers

--- UndertaleModLib/Properties/AssemblyInfo.cs
+++ UndertaleModLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.19")]
-[assembly: AssemblyFileVersion("0.1.19")]
+[assembly: AssemblyVersion("0.1.20")]
+[assembly: AssemblyFileVersion("0.1.20")]
