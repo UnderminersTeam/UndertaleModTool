601b0cdcd67c88deb07e2df3dfd15a3279626027
Kneesnap <davidmenlo2@gmail.com>
2019-11-20 00:43:59 -0800

    AssemblyTree works properly now.

--- UndertaleModLib/Decompiler/AssemblyTree.cs
+++ UndertaleModLib/Decompiler/AssemblyTree.cs
@@ -18,6 +18,7 @@ namespace UndertaleModLib.Decompiler
         public bool IsNextWithBlock { get; set; }
         public AssemblyTreeNode ConditionFailNode { get; set; }
         public bool IsConditional { get; set; }
+        public bool IsConditionSwapped { get; set; }
         public uint Address { get => Block.Address; }
 
         public AssemblyTreeNode(AssemblyTreeNode parent, Block code)
@@ -34,6 +35,9 @@ namespace UndertaleModLib.Decompiler
         public AssemblyTreeNode Root { get; set; }
         public Dictionary<uint, Block> Blocks { get; set; } = new Dictionary<uint, Block>();
         public Dictionary<uint, AssemblyTreeNode> Nodes { get; set; } = new Dictionary<uint, AssemblyTreeNode>();
+        public List<uint> BlockStarts = new List<uint>();
+        public Dictionary<uint, UndertaleInstruction> BlockAddresses = new Dictionary<uint, UndertaleInstruction>();
+        public uint MaxAddress;
 
         public void AddNode(AssemblyTreeNode newNode)
         {
@@ -57,8 +61,8 @@ namespace UndertaleModLib.Decompiler
                 foreach (var instr in handleNode.Block.Instructions)
                     builder.Append(instr.ToString() + "\n");
 
+                nodeQueue.Enqueue(handleNode.ConditionFailNode); // The jump will always jump below the jump fail code.
                 nodeQueue.Enqueue(handleNode.Next);
-                nodeQueue.Enqueue(handleNode.ConditionFailNode);
             }
 
             return builder.ToString();
@@ -80,7 +84,17 @@ namespace UndertaleModLib.Decompiler
 
                 var block = node.Block;
                 sb.Append("    block_" + block.Address + " [label=\"");
-                sb.Append("[" + block.ToString() + (node.IsConditional ? ", Conditional" : "") + (node.Next != null ? ", T: " + node.Next.Address : "") + (node.ConditionFailNode != null ? ", F: " + node.ConditionFailNode.Address : "") + "]\n");
+                bool SwapCondition = node.IsConditional && node.IsConditionSwapped;
+                sb.Append("[" + block.ToString() + (node.IsConditional ? ", Conditional" : ""));
+
+                AssemblyTreeNode trueNode = SwapCondition ? node.ConditionFailNode : node.Next;
+                AssemblyTreeNode falseNode = SwapCondition ? node.Next : node.ConditionFailNode;
+                if (trueNode != null)
+                    sb.Append(", T: " + trueNode.Address);
+                if (falseNode != null)
+                    sb.Append(", F: " + falseNode.Address);
+
+                sb.Append("]\n");
                 foreach (var instr in block.Instructions)
                     sb.Append(instr.ToString().Replace("\"", "\\\"") + "\\n");
                 sb.Append("\"");
@@ -103,10 +117,11 @@ namespace UndertaleModLib.Decompiler
                 var block = node.Block;
                 if (node.IsConditional)
                 {
+                    bool SwapCondition = node.IsConditional && node.IsConditionSwapped;
                     if (node.Next != null)
-                        sb.AppendLine("    block_" + block.Address + " -> block_" + node.Next.Address + " [color=\"green\"];");
+                        sb.AppendLine("    block_" + block.Address + " -> block_" + node.Next.Address + " [color=\"" + (SwapCondition ? "red" : "green") + "\"];");
                     if (node.ConditionFailNode != null)
-                        sb.AppendLine("    block_" + block.Address + " -> block_" + node.ConditionFailNode.Address + " [color=\"red\"];");
+                        sb.AppendLine("    block_" + block.Address + " -> block_" + node.ConditionFailNode.Address + " [color=\"" + (SwapCondition ? "green" : "red") + "\"];");
                 }
                 else if (node.Next != null)
                         sb.AppendLine("    block_" + block.Address + " -> block_" + node.Next.Address + ";");
@@ -124,19 +139,27 @@ namespace UndertaleModLib.Decompiler
             if (tree.Blocks.ContainsKey(startAddress))
                 return tree.Nodes[startAddress]; // Prevent reading multiple times.
 
-            UndertaleCode code = tree.Context.TargetCode;
             Block currentBlock = new Block(startAddress);
-            for (uint i = startAddress; i < code.Instructions.Count; i++)
+            for (uint i = startAddress; i <= tree.MaxAddress;)
             {
-                UndertaleInstruction instr = code.Instructions[(int) i];
+                UndertaleInstruction instr = tree.BlockAddresses[i];
+                if (tree.Blocks.ContainsKey(instr.Address)) { // Using some already existing block, while also having instructions before it.
+                    AssemblyTreeNode newNode = new AssemblyTreeNode(parent, currentBlock);
+                    tree.AddNode(newNode);
+                    if (parent != null)
+                        parent.Next = newNode;
+                    newNode.Next = tree.Nodes[instr.Address];
+                    return newNode;
+                }
+
                 currentBlock.Instructions.Add(instr); // Add instruction.
 
                 if (instr.Kind == UndertaleInstruction.Opcode.B)
                 {
                     AssemblyTreeNode addNode = new AssemblyTreeNode(parent, currentBlock);
+                    tree.AddNode(addNode);
                     if (parent != null)
                         parent.Next = addNode;
-                    tree.AddNode(addNode);
 
                     addNode.Next = ReadNode(tree, (uint) (instr.Address + instr.JumpOffset), addNode);
                     return addNode;
@@ -144,25 +167,22 @@ namespace UndertaleModLib.Decompiler
                 else if (instr.Kind == UndertaleInstruction.Opcode.Bt || instr.Kind == UndertaleInstruction.Opcode.Bf)
                 {
                     AssemblyTreeNode addNode = new AssemblyTreeNode(parent, currentBlock);
+                    tree.AddNode(addNode);
                     if (parent != null)
                         parent.Next = addNode;
-                    tree.AddNode(addNode);
 
-
-                    AssemblyTreeNode stayNode = ReadNode(tree, instr.Address + 1, addNode);
-                    AssemblyTreeNode jumpNode = ReadNode(tree, (uint)(instr.Address + instr.JumpOffset), addNode);
                     addNode.IsConditional = true;
-                    addNode.ConditionFailNode = (instr.Kind == UndertaleInstruction.Opcode.Bt ? jumpNode : stayNode);
-                    addNode.ConditionFailNode = (instr.Kind == UndertaleInstruction.Opcode.Bt ? stayNode : jumpNode);
-                    //TODO: addNode.next should go where these meet up.
+                    addNode.IsConditionSwapped = (instr.Kind == UndertaleInstruction.Opcode.Bf);
+                    addNode.ConditionFailNode = ReadNode(tree, instr.Address + 1, addNode); // The next block is just after this instruction.
+                    addNode.Next = ReadNode(tree, (uint)(instr.Address + instr.JumpOffset), addNode); // The next block is where it jumps to.
                     return addNode;
                 }
                 else if (instr.Kind == UndertaleInstruction.Opcode.PushEnv || instr.Kind == UndertaleInstruction.Opcode.PopEnv)
                 {
                     AssemblyTreeNode addNode = new AssemblyTreeNode(parent, currentBlock);
+                    tree.AddNode(addNode);
                     if (parent != null)
                         parent.Next = addNode;
-                    tree.AddNode(addNode);
 
                     addNode.Next = ReadNode(tree, (instr.Address + 1), addNode);
                     addNode.IsNextWithBlock = true;
@@ -170,12 +190,21 @@ namespace UndertaleModLib.Decompiler
                 }
                 else if (instr.Kind == UndertaleInstruction.Opcode.Ret || instr.Kind == UndertaleInstruction.Opcode.Exit)
                 {
-                    AssemblyTreeNode addNode = new AssemblyTreeNode(parent, currentBlock);
-                    if (parent != null)
-                        parent.Next = addNode;
-                    tree.AddNode(addNode);
-                    return addNode;
+                    break;
                 }
+
+                uint nextAddress = instr.Address + instr.CalculateInstructionSize();
+                if (tree.BlockStarts.Contains(nextAddress)) // The next instruction is the start of a new block, read it as one.
+                {
+                    AssemblyTreeNode newNode = new AssemblyTreeNode(parent, currentBlock);
+                    tree.AddNode(newNode);
+                    if (parent != null)
+                        parent.Next = newNode;
+                    newNode.Next = ReadNode(tree, nextAddress, newNode);
+                    return newNode;
+                }
+
+                i = nextAddress; // Move to the next instruction.
             }
 
             AssemblyTreeNode lastNode = new AssemblyTreeNode(parent, currentBlock); // Reached the end of the code.
@@ -185,10 +214,42 @@ namespace UndertaleModLib.Decompiler
             return lastNode;
         }
 
+        public void Setup(DecompileContext context)
+        {
+            Context = context;
+
+            // Calculate BlockStarts.
+            BlockStarts.Clear();
+            for (uint i = 0; i < Context.TargetCode.Instructions.Count; i++)
+            {
+                UndertaleInstruction instr = Context.TargetCode.Instructions[(int)i];
+
+                if (instr.Kind == UndertaleInstruction.Opcode.B)
+                {
+                    BlockStarts.Add((uint)(instr.Address + instr.JumpOffset));
+                }
+                else if (instr.Kind == UndertaleInstruction.Opcode.Bt || instr.Kind == UndertaleInstruction.Opcode.Bf)
+                {
+                    BlockStarts.Add(instr.Address + instr.CalculateInstructionSize());
+                    BlockStarts.Add((uint)(instr.Address + instr.JumpOffset));
+                }
+            }
+
+            // Setup instruction address map.
+            BlockAddresses.Clear();
+            for (int i = 0; i < context.TargetCode.Instructions.Count; i++)
+            {
+                UndertaleInstruction instr = context.TargetCode.Instructions[i];
+                BlockAddresses[instr.Address] = instr;
+            }
+
+            MaxAddress = context.TargetCode.Instructions.Last().Address;
+        }
+
         public static AssemblyTree CreateTree(DecompileContext context)
         {
             AssemblyTree newTree = new AssemblyTree();
-            newTree.Context = context;
+            newTree.Setup(context);
             newTree.Root = ReadNode(newTree, 0, null);
             return newTree;
         }
