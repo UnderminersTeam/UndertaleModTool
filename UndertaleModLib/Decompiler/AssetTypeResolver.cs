5996bcb4c7717340284ceaa5465a2548abf34f69
Nikita Krapivin <alienoom@yandex.ru>
2020-01-22 15:44:47 +0500

    Add os_winphone to OSType enum

--- UndertaleModLib/Decompiler/AssetTypeResolver.cs
+++ UndertaleModLib/Decompiler/AssetTypeResolver.cs
@@ -55,26 +55,28 @@ namespace UndertaleModLib.Decompiler
     public enum OSType : int
     {
         os_windows = 0,
-        os_uwp = 18,
-        os_linux = 6,
         os_macosx = 1,
+        os_psp = 2,
         os_ios = 3,
         os_android = 4,
-        os_ps3 = 16,
-        os_ps4 = 14,
-        os_psvita = 12,
-        os_xboxone = 15,
-        os_unknown = -1,
-        os_3ds = 11,
-        os_bb10 = 13,
-        os_psp = 2,
         os_symbian = 5,
+        os_linux = 6,
+        os_winphone = 7,
         os_tizen = 8,
-        os_wiiu = 10,
         os_win8native = 9,
+        os_wiiu = 10,
+        os_3ds = 11,
+        os_psvita = 12,
+        os_bb10 = 13,
+        os_ps4 = 14,
+        os_xboxone = 15,
+        os_ps3 = 16,
         os_xbox360 = 17,
-        os_switch_beta = 20, // this one was used while switch support was in beta and changed later?
+        os_uwp = 18,
+        // constant number 19 is missing... no idea why it was skipped...
+        os_switch_beta = 20, // this one was used while switch support was in beta and changed later? In newer runtimes 20 is now os_tvos...
         os_switch = 21,
+        os_unknown = -1
     }
 
     public enum GamepadButton : int
