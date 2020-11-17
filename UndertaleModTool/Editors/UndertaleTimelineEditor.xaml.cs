f1551bf91ef0b7238c82e6f8e2eed08776316243
Nikita Krapivin <alienoom@yandex.ru>
2020-11-17 22:16:38 +0500

    Implement timeline editor, and start working on the extension editor.

--- UndertaleModTool/Editors/UndertaleTimelineEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleTimelineEditor.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UndertaleModLib;
+using UndertaleModLib.Models;
 
 namespace UndertaleModTool
 {
@@ -23,6 +25,25 @@ namespace UndertaleModTool
         public UndertaleTimelineEditor()
         {
             InitializeComponent();
+        }
+
+        private void DataGrid_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            UndertaleTimeline.UndertaleTimelineMoment obj = new UndertaleTimeline.UndertaleTimelineMoment();
+
+            // find the last timeline moment (which should have the biggest step value)
+            var lastMoment = ((sender as DataGrid).ItemsSource as IList<UndertaleTimeline.UndertaleTimelineMoment>).LastOrDefault();
+
+            // the default value is 0 anyway.
+            if (lastMoment != null)
+                obj.Step = lastMoment.Step + 1;
+
+            // make an empty event with a null code entry.
+            obj.Event = new UndertalePointerList<UndertaleGameObject.EventAction>();
+            obj.Event.Add(new UndertaleGameObject.EventAction());
+
+            // we're done here.
+            e.NewItem = obj;
         }
     }
 }
