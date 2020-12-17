046c76db7bf0fdba91fba34a77702235cc9e4b5d
Grossley <42724744+Grossley@users.noreply.github.com>
2020-12-17 00:01:47 -0800

    Pre-emptively bump version number

--- UndertaleModTool/Properties/AssemblyInfo.cs
+++ UndertaleModTool/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.5.4")]
-[assembly: AssemblyFileVersion("0.3.5.4")]
+[assembly: AssemblyVersion("0.3.5.5")]
+[assembly: AssemblyFileVersion("0.3.5.5")]
