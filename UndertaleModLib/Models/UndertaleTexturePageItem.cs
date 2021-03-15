df1cf29c72c10ff114db32cc1929e556937c0572
Grossley <42724744+Grossley@users.noreply.github.com>
2021-03-15 14:04:25 -0700

    Bandaid fix null name causing crash error for non-named assets

--- UndertaleModLib/Models/UndertaleTexturePageItem.cs
+++ UndertaleModLib/Models/UndertaleTexturePageItem.cs
@@ -74,6 +74,14 @@ namespace UndertaleModLib.Models
 
         public override string ToString()
         {
+            try
+            {
+                return Name.Content + " (" + GetType().Name + ")";
+            }
+            catch
+            {
+                Name = new UndertaleString("PageItem Unknown Index");
+            }
             return Name.Content + " (" + GetType().Name + ")";
         }
 
