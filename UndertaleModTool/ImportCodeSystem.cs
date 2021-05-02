ec4987b4e0b4feca46f9cef92da429c695975f0c
Grossley <42724744+Grossley@users.noreply.github.com>
2021-05-02 00:17:08 -0700

    Fix NukeProfileGML

--- UndertaleModTool/ImportCodeSystem.cs
+++ UndertaleModTool/ImportCodeSystem.cs
@@ -58,7 +58,12 @@ namespace UndertaleModTool
         {
             string path = Path.Combine(ProfilesFolder, Data.ToolInfo.CurrentMD5, "Temp", codeName + ".gml");
             if (File.Exists(path))
+            {
+                //The file must be deleted to write it again properly with true decompiled output
+                //Just overwriting it isn't enough
+                File.Delete(path);
                 File.WriteAllText(path, GetDecompiledText(codeName));
+            }
         }
 
         public string GetPassBack(string decompiled_text, string keyword, string replacement, bool case_sensitive = false)
