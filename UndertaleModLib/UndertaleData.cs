c6965032558437e602afb6f40280b097ef729e1d
colinator27 <17358554+colinator27@users.noreply.github.com>
2021-12-28 18:57:04 -0500

    Add new QOI+BZip2 image format support
    Note: Currently performs slowly when saving/loading, but is compatible with WPF and existing code. May need to refactor everything else later.

--- UndertaleModLib/UndertaleData.cs
+++ UndertaleModLib/UndertaleData.cs
@@ -82,6 +82,7 @@ namespace UndertaleModLib
         public bool GMS2_3 = false;
         public bool GMS2_3_1 = false;
         public bool GMS2_3_2 = false;
+        public bool UseQoiFormat = false;
         public ToolInfo ToolInfo = new ToolInfo();
         public int PaddingAlignException = -1;
 
