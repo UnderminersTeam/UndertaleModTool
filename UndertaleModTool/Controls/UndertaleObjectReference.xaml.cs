a9a07d94f948abfa2e9c8bced0a07e63def466f0
NyakoFox <nyakowofox@gmail.com>
2022-03-04 15:07:09 -0400

    Attempt to add an "add code" button to places that reference UndertaleCode

--- UndertaleModTool/Controls/UndertaleObjectReference.xaml.cs
+++ UndertaleModTool/Controls/UndertaleObjectReference.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using UndertaleModLib;
+using UndertaleModLib.Models;
+using UndertaleModLib.Decompiler;
+using UndertaleModLib.Scripting;
 
 namespace UndertaleModTool
 {
@@ -39,6 +42,13 @@ namespace UndertaleModTool
                 new FrameworkPropertyMetadata(true,
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        public static DependencyProperty EventSuffixProperty =
+            DependencyProperty.Register("EventSuffix", typeof(string),
+                typeof(UndertaleObjectReference),
+                new FrameworkPropertyMetadata("",
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
         public object ObjectReference
         {
             get { return GetValue(ObjectReferenceProperty); }
@@ -57,6 +67,12 @@ namespace UndertaleModTool
             set { SetValue(ObjectTypeProperty, value); }
         }
 
+        public string EventSuffix
+        {
+            get { return (string)GetValue(EventSuffixProperty); }
+            set { SetValue(EventSuffixProperty, value); }
+        }
+
         public UndertaleObjectReference()
         {
             InitializeComponent();
@@ -64,7 +80,68 @@ namespace UndertaleModTool
 
         private void Details_Click(object sender, RoutedEventArgs e)
         {
-            (Application.Current.MainWindow as MainWindow).ChangeSelection(ObjectReference);
+            if (ObjectReference is null)
+            {
+                MessageBox.Show("This feature is very WIP, so expect it to be broken.");
+
+                MainWindow mainWindow = (Application.Current.MainWindow as MainWindow);
+
+                if (mainWindow.Selected is null)
+                {
+                    MessageBox.Show("Nothing currently selected! This is currently unsupported.");
+                    return;
+                }
+                string name = (mainWindow.Selected as UndertaleNamedResource).Name.Content;
+
+                string prefix = "gml_";
+
+                switch (mainWindow.Selected)
+                {
+                    case UndertaleGameObject: prefix += "Object"; break;
+                    case UndertaleRoom:       prefix += "RoomCC"; break;
+                    case UndertaleScript:     prefix += "Script"; break;
+                }
+
+                prefix += "_";
+
+                //MessageBox.Show(mainWindow.Selected.GetType().Name);
+
+
+                // TEMPORARILY use ImportGMLString... this sucks.
+                //mainWindow.ImportGMLString(prefix + name + "_" + EventSuffix + "_0", "", true, false);
+                //UndertaleCode code = mainWindow.Data.Code.ByName(prefix + name + "_" + EventSuffix + "_0");
+
+                //if (code is null)
+                //{
+                //    MessageBox.Show("This should never happen.");
+                //    return;
+                //}
+
+
+                // Okay, INSTEAD of using ImportGMLString, let's do it ourselves!
+                UndertaleCode code = new UndertaleCode();
+                code.Name = mainWindow.Data.Strings.MakeString(prefix + name + "_" + EventSuffix + "_0");
+                mainWindow.Data.Code.Add(code);
+                if (mainWindow.Data?.GeneralInfo.BytecodeVersion > 14)
+                {
+                    UndertaleCodeLocals locals = new UndertaleCodeLocals();
+                    locals.Name = code.Name;
+                    UndertaleCodeLocals.LocalVar argsLocal = new UndertaleCodeLocals.LocalVar();
+                    argsLocal.Name = mainWindow.Data.Strings.MakeString("arguments");
+                    argsLocal.Index = 0;
+                    locals.Locals.Add(argsLocal);
+                    code.LocalsCount = 1;
+                    code.GenerateLocalVarDefinitions(code.FindReferencedLocalVars(), locals);
+                    mainWindow.Data.CodeLocals.Add(locals);
+                }
+
+                // TODO: This doesn't persist somehow?
+                ObjectReference = code;
+            }
+            else
+            {
+                (Application.Current.MainWindow as MainWindow).ChangeSelection(ObjectReference);
+            }
         }
 
         private void Remove_Click(object sender, RoutedEventArgs e)
