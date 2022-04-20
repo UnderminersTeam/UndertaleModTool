3c882bc06338cb223dc752f937c0173c3bbd2f81
VladiStep <vlad2001.21@mail.ru>
2022-04-20 15:42:37 +0300

    Added missing "Frame index" and "Image speed" fields for game object instances; added "DataFieldOneTimeConverter"; added missing "IsSharedSizeScope" attribute

---
+++ UndertaleModTool/Converters/DataFieldOneTimeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using UndertaleModLib;
+
+namespace UndertaleModTool
+{
+    public class DataFieldOneTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not UndertaleData data || parameter is not string par)
+                return null;
+
+            FieldInfo info = data.GetType().GetField(par);
+            object resObj = info?.GetValue(data);
+
+            if (resObj is bool res)
+                return res ? Visibility.Visible : Visibility.Collapsed;
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
