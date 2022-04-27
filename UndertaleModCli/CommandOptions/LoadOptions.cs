a3954f1d4cc218c9f3b51bc7438bc0027725e765
Miepee <38186597+Miepee@users.noreply.github.com>
2022-04-27 20:09:49 +0200

    Cleans up PR 860
    Co-Authored-By: VladiStep <vlad2001.21@mail.ru>

---
+++ UndertaleModCli/CommandOptions/LoadOptions.cs
@@ -0,0 +1,40 @@
+using System.IO;
+
+namespace UndertaleModCli
+{
+	/// <summary>
+	/// Cli options for the Load command
+	/// </summary>
+	public class LoadOptions
+	{
+		/// <summary>
+		/// File path to the data file
+		/// </summary>
+		public FileInfo Datafile { get; set; }
+
+		/// <summary>
+		/// File paths to the scripts that shall be run
+		/// </summary>
+		public FileInfo[] Scripts { get; set; }
+
+		/// <summary>
+		/// C# string that shall be executed
+		/// </summary>
+		public string? Line { get; set; }
+
+		/// <summary>
+		/// File path to where to save the modified data file
+		/// </summary>
+		public FileInfo? Output { get; set; }
+
+		/// <summary>
+		/// Determines if Cli should be run in interactive mode
+		/// </summary>
+		public bool Interactive { get; set; } = false;
+
+		/// <summary>
+		/// Determines if Cli should print out verbose logs
+		/// </summary>
+		public bool Verbose { get; set; } = false;
+	}
+}
