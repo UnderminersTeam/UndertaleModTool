3d213a98799850ad6729fb0cf801d41500d461da
VladiStep <vlad2001.21@mail.ru>
2022-06-07 16:51:57 +0300

    Optimized string name displaying in the "Strings" list - character limit is set to 256, and newlines are replaced with spaces.

--- UndertaleModTool/MainWindow.xaml.cs
+++ UndertaleModTool/MainWindow.xaml.cs
@@ -3616,4 +3616,24 @@ result in loss of work.");
             Description = description;
         }
     }
+
+    public class StringTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string str)
+                return null;
+
+            if (str.Length > 256)
+                str = str[..256] + "...";
+            str = str.Replace('\n', ' ');
+
+            return str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
