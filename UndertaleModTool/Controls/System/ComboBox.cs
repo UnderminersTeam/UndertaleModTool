781fdde0b533dfc1d63d8e48c7621c28b1aad1e8
VladiStep <vlad2001.21@mail.ru>
2022-12-24 00:15:48 +0300

    Fixed custom button text color, replaced all combo boxes with new ones.

--- UndertaleModTool/Controls/System/ComboBox.cs
+++ UndertaleModTool/Controls/System/ComboBox.cs
@@ -4,16 +4,16 @@ using System.Windows.Media;
 namespace UndertaleModTool
 {
     /// <summary>
-    /// A standard button which compatible with the dark mode.
+    /// A standard combo box which compatible with the dark mode.
     /// </summary>
-    public partial class Button : System.Windows.Controls.Button
+    public partial class ComboBox : System.Windows.Controls.ComboBox
     {
         // Setting "Foreground" implicitly breaks internal "IsEnabled" style trigger,
         // so this has to be handled manually.
         private static readonly SolidColorBrush disabledTextBrush = new(Color.FromArgb(255, 131, 131, 131));
 
-        /// <summary>Initializes a new instance of the button.</summary>
-        public Button()
+        /// <summary>Initializes a new instance of the combo box.</summary>
+        public ComboBox()
         {
             // Even though this will be called again in "OnPropertyChanged()", it's required.
             SetResourceReference(ForegroundProperty, "CustomTextBrush");
