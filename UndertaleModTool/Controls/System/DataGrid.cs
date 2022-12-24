b8a63b9239398705a6bd1c869022106f660a4b72
VladiStep <vlad2001.21@mail.ru>
2022-12-24 15:19:28 +0300

    Added custom "DataGrid" and replaced all the occurences.

---
+++ UndertaleModTool/Controls/System/DataGrid.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace UndertaleModTool
+{
+    /// <summary>
+    /// A standard data grid which compatible with the dark mode.
+    /// </summary>
+    public partial class DataGrid : System.Windows.Controls.DataGrid
+    {
+        /// <summary>Initializes a new instance of the data grid.</summary>
+        public DataGrid()
+        {
+            Loaded += DataGrid_Loaded;
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var pres = MainWindow.FindVisualChild<DataGridColumnHeadersPresenter>(this);
+            if (pres is null)
+                return;
+
+            pres.SetResourceReference(ForegroundProperty, "CustomTextBrush");
+        }
+    }
+}
