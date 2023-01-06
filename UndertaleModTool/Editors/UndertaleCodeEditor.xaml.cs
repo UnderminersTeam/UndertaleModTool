ffcdbb7267a474d641bdcd806c2992cf6532b4c6
VladiStep <vlad2001.21@mail.ru>
2023-01-06 16:23:10 +0300

    Fixed #1147

--- UndertaleModTool/Editors/UndertaleCodeEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleCodeEditor.xaml.cs
@@ -231,7 +231,7 @@ namespace UndertaleModTool
                 CurrentDecompiled is not null && CurrentDecompiled != code)
             {
                 DecompiledSkipped = true;
-                DecompiledEditor_LostFocus(sender, null);
+                await DecompiledLostFocusBody(sender, null);
 
             }
             else if (DisassemblyTab.IsSelected && DisassemblyFocused && DisassemblyChanged &&
@@ -241,7 +241,7 @@ namespace UndertaleModTool
                 DisassemblyEditor_LostFocus(sender, null);
             }
 
-            DecompiledEditor_LostFocus(sender, null);
+            await DecompiledLostFocusBody(sender, null);
             DisassemblyEditor_LostFocus(sender, null);
 
             if (MainWindow.CodeEditorDecompile != Unstated) //if opened from the code search results "link"
