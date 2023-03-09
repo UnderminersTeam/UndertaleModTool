61833f28ddc8004c8db5fb6537b5fa4ff0d5cbac
Grossley <42724744+Grossley@users.noreply.github.com>
2023-03-09 09:24:36 -0800

    Bump the current version to be 0.5.1.0
    Bump the current version to be 0.5.1.0

--- UndertaleModLib/Properties/AssemblyInfo.cs
+++ UndertaleModLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
