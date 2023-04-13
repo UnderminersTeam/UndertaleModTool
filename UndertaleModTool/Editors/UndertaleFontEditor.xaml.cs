092673d31d5be05c8ec482b27d10a04e4244bad4
VladiStep <vlad2001.21@mail.ru>
2023-04-13 16:23:31 +0300

    Make the font texture clickable.

--- UndertaleModTool/Editors/UndertaleFontEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleFontEditor.xaml.cs
@@ -21,6 +21,8 @@ namespace UndertaleModTool
     /// </summary>
     public partial class UndertaleFontEditor : DataUserControl
     {
+        private static readonly MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+
         public UndertaleFontEditor()
         {
             InitializeComponent();
@@ -36,6 +38,48 @@ namespace UndertaleModTool
             font.Glyphs.Clear();
             foreach (var glyph in copy)
                 font.Glyphs.Add(glyph);
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is not UndertaleFont font)
+                return;
+
+            var pos = e.GetPosition(sender as IInputElement);
+            for (int i = 0; i < font.Glyphs.Count; i++)
+            {
+                var glyph = font.Glyphs[i];
+
+                if (pos.X > glyph.SourceX && pos.X < glyph.SourceX + glyph.SourceWidth
+                    && pos.Y > glyph.SourceY && pos.Y < glyph.SourceY + glyph.SourceHeight)
+                {
+                    GlyphsGrid.SelectedItem = glyph;
+
+                    // "ScrollIntoView(glyph)" is noticeably slower
+                    ScrollGlyphIntoView(i);
+                    break;
+                }
+            }
+        }
+        private void ScrollGlyphIntoView(int glyphIndex)
+        {
+            ScrollViewer glyphListViewer = MainWindow.FindVisualChild<ScrollViewer>(GlyphsGrid);
+            if (glyphListViewer is null)
+            {
+                mainWindow.ShowError("Cannot find the glyphs table scroll viewer.");
+                return;
+            }
+            glyphListViewer.ScrollToVerticalOffset(glyphIndex + 1 - (glyphListViewer.ViewportHeight / 2)); // DataGrid offset is logical
+            glyphListViewer.UpdateLayout();
+
+            ScrollViewer dataEditorViewer = mainWindow.DataEditor.Parent as ScrollViewer;
+            if (dataEditorViewer is not null)
+            {
+                double initOffset = dataEditorViewer.VerticalOffset;
+
+                dataEditorViewer.UpdateLayout();
+                dataEditorViewer.ScrollToVerticalOffset(initOffset);
+            }
         }
     }
 }
