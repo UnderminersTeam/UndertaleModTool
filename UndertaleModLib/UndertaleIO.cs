b9637533060cd9717e27c97cf2ae01e946b398a5
Nikita Krapivin <alienoom@yandex.ru>
2023-10-31 21:52:36 +0500

    Update UndertaleIO.cs

--- UndertaleModLib/UndertaleIO.cs
+++ UndertaleModLib/UndertaleIO.cs
@@ -344,9 +344,7 @@ namespace UndertaleModLib
             if (poolSize != 0 && poolSize != objectPool.Count)
             {
                 SubmitWarning("Warning - the estimated object pool size differs from the actual size.\n" +
-                              "Please report this on UndertaleModTool GitHub.\n" +
-                              "Unless you have a .win with SWF sprites,\n" +
-                              "since YYSWF objects are not pooled due to overlapping.");
+                              "Please report this on UndertaleModTool GitHub.");
             }
 
             return false;
