9e4a593153846c59f3cc8e67d31b0e9a5d8af37f
Miepee <janbidler00@protonmail.com>
2023-11-15 23:52:00 +0100

    Clean up usings

--- UndertaleModLib/UndertaleLists.cs
+++ UndertaleModLib/UndertaleLists.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.IO;
 using System.Reflection;
 using UndertaleModLib.Models;
