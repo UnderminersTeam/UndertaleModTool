b4dd901e08e41697edec2a99a1985b9046bcd0c6
colinator27 <17358554+colinator27@users.noreply.github.com>
2024-07-13 15:58:25 -0400

    Address reviews

--- UndertaleModLib/UndertaleChunks.cs
+++ UndertaleModLib/UndertaleChunks.cs
@@ -453,7 +453,7 @@ namespace UndertaleModLib
                 uint nextSpritePtr = 0;
                 if ((i + 1) < spriteCount)
                     nextSpritePtr = reader.ReadUInt32();
-                reader.AbsPosition = spritePtr + 4 /* skip name */;
+                reader.AbsPosition = spritePtr + 4; // Skip past "Name"
 
                 // Check if bbox size differs from width/height
                 uint width = reader.ReadUInt32();
@@ -472,106 +472,101 @@ namespace UndertaleModLib
                 
                 reader.Position += 28;
 
-                if (reader.ReadInt32() == -1)
-                {
-                    uint sVersion = reader.ReadUInt32();
-                    UndertaleSprite.SpriteType sSpriteType = (UndertaleSprite.SpriteType)reader.ReadUInt32();
-
-                    if (sSpriteType != UndertaleSprite.SpriteType.Normal)
-                    {
-                        // We can't determine anything from this sprite
-                        continue;
-                    }
-
-                    reader.Position += 8; // playback speed values
-
-                    if (sVersion != 3)
-                    {
-                        throw new IOException("Expected sprite version 3");
-                    }
-                    uint sequenceOffset = reader.ReadUInt32();
-                    uint nineSliceOffset = reader.ReadUInt32();
-
-                    // Skip past texture pointers
-                    uint textureCount = reader.ReadUInt32();
-                    reader.Position += textureCount * 4;
-
-                    // Calculate how much space the "full" and "bbox" mask data take up
-                    uint maskCount = reader.ReadUInt32();
-                    if (maskCount == 0)
-                    {
-                        // We can't determine anything from this sprite
-                        continue;
-                    }
-                    uint fullLength = (normalWidth + 7) / 8 * normalHeight;
-                    fullLength *= maskCount;
-                    if ((fullLength % 4) != 0)
-                        fullLength += (4 - (fullLength % 4));
-                    uint bboxLength = (bboxWidth + 7) / 8 * bboxHeight;
-                    bboxLength *= maskCount;
-                    if ((bboxLength % 4) != 0)
-                        bboxLength += (4 - (bboxLength % 4));
-
-                    // Calculate expected end offset
-                    long expectedEndOffset;
-                    bool endOffsetLenient = false;
-                    if (sequenceOffset != 0)
-                    {
-                        expectedEndOffset = sequenceOffset;
-                    }
-                    else if (nineSliceOffset != 0)
-                    {
-                        expectedEndOffset = nineSliceOffset;
-                    }
-                    else if (nextSpritePtr != 0)
-                    {
-                        expectedEndOffset = nextSpritePtr;
-                    }
-                    else
-                    {
-                        // Use chunk length, and be lenient with it (due to chunk padding)
-                        endOffsetLenient = true;
-                        expectedEndOffset = chunkStartPos + Length;
-                    }
-
-                    // If the "full" mask data runs past the expected end offset, and the "bbox" mask data does not, then this is 2024.6.
-                    // Otherwise, stop processing and assume this is not 2024.6.
-                    long fullEndPos = (reader.AbsPosition + fullLength);
-                    if (fullEndPos != expectedEndOffset)
-                    {
-                        if (endOffsetLenient && (fullEndPos % 16) != 0 && fullEndPos + (16 - (fullEndPos % 16)) == expectedEndOffset)
-                        {
-                            // "Full" mask data doesn't exactly line up, but works if rounded up to the next chunk padding
-                            break;
-                        }
-
-                        long bboxEndPos = (reader.AbsPosition + bboxLength);
-                        if (bboxEndPos == expectedEndOffset)
-                        {
-                            // "Bbox" mask data is valid
-                            reader.undertaleData.SetGMS2Version(2024, 6);
-                        }
-                        else if (endOffsetLenient && (bboxEndPos % 16) != 0 && bboxEndPos + (16 - (bboxEndPos % 16)) == expectedEndOffset)
-                        {
-                            // "Bbox" mask data doesn't exactly line up, but works if rounded up to the next chunk padding
-                            reader.undertaleData.SetGMS2Version(2024, 6);
-                        }
-                        else
-                        {
-                            // Neither option seems to have worked...
-                            throw new IOException("Failed to detect mask type in 2024.6 detection");
-                        }
-                    }
-                    else
-                    {
-                        // "Full" mask data is valid
-                        break;
-                    }
-                }
-                else
+                if (reader.ReadInt32() != -1)
                 {
                     throw new IOException("Expected special sprite type");
                 }
+
+                uint sVersion = reader.ReadUInt32();
+                UndertaleSprite.SpriteType sSpriteType = (UndertaleSprite.SpriteType)reader.ReadUInt32();
+
+                if (sSpriteType != UndertaleSprite.SpriteType.Normal)
+                {
+                    // We can't determine anything from this sprite
+                    continue;
+                }
+
+                reader.Position += 8; // Playback speed values
+
+                if (sVersion != 3)
+                {
+                    throw new IOException("Expected sprite version 3");
+                }
+                uint sequenceOffset = reader.ReadUInt32();
+                uint nineSliceOffset = reader.ReadUInt32();
+
+                // Skip past texture pointers
+                uint textureCount = reader.ReadUInt32();
+                reader.Position += textureCount * 4;
+
+                // Calculate how much space the "full" and "bbox" mask data take up
+                uint maskCount = reader.ReadUInt32();
+                if (maskCount == 0)
+                {
+                    // We can't determine anything from this sprite
+                    continue;
+                }
+                uint fullLength = (normalWidth + 7) / 8 * normalHeight;
+                fullLength *= maskCount;
+                if ((fullLength % 4) != 0)
+                    fullLength += (4 - (fullLength % 4));
+                uint bboxLength = (bboxWidth + 7) / 8 * bboxHeight;
+                bboxLength *= maskCount;
+                if ((bboxLength % 4) != 0)
+                    bboxLength += (4 - (bboxLength % 4));
+
+                // Calculate expected end offset
+                long expectedEndOffset;
+                bool endOffsetLenient = false;
+                if (sequenceOffset != 0)
+                {
+                    expectedEndOffset = sequenceOffset;
+                }
+                else if (nineSliceOffset != 0)
+                {
+                    expectedEndOffset = nineSliceOffset;
+                }
+                else if (nextSpritePtr != 0)
+                {
+                    expectedEndOffset = nextSpritePtr;
+                }
+                else
+                {
+                    // Use chunk length, and be lenient with it (due to chunk padding)
+                    endOffsetLenient = true;
+                    expectedEndOffset = chunkStartPos + Length;
+                }
+
+                // If the "full" mask data runs past the expected end offset, and the "bbox" mask data does not, then this is 2024.6.
+                // Otherwise, stop processing and assume this is not 2024.6.
+                long fullEndPos = (reader.AbsPosition + fullLength);
+                if (fullEndPos == expectedEndOffset)
+                {
+                    // "Full" mask data is valid
+                    break;
+                }
+                if (endOffsetLenient && (fullEndPos % 16) != 0 && fullEndPos + (16 - (fullEndPos % 16)) == expectedEndOffset)
+                {
+                    // "Full" mask data doesn't exactly line up, but works if rounded up to the next chunk padding
+                    break;
+                }
+
+                long bboxEndPos = (reader.AbsPosition + bboxLength);
+                if (bboxEndPos == expectedEndOffset)
+                {
+                    // "Bbox" mask data is valid
+                    reader.undertaleData.SetGMS2Version(2024, 6);
+                    break;
+                }
+                if (endOffsetLenient && (bboxEndPos % 16) != 0 && bboxEndPos + (16 - (bboxEndPos % 16)) == expectedEndOffset)
+                {
+                    // "Bbox" mask data doesn't exactly line up, but works if rounded up to the next chunk padding
+                    reader.undertaleData.SetGMS2Version(2024, 6);
+                    break;
+                }
+
+                // Neither option seems to have worked...
+                throw new IOException("Failed to detect mask type in 2024.6 detection");
             }
 
             reader.Position = returnTo;
