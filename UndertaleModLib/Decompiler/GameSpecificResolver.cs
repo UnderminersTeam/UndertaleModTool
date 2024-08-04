298514ae36074233a9d45e4f8421724eb20db6db
colinator27 <17358554+colinator27@users.noreply.github.com>
2024-08-03 20:10:34 -0400

    Externalize game-specific data

--- UndertaleModLib/Decompiler/GameSpecificResolver.cs
+++ UndertaleModLib/Decompiler/GameSpecificResolver.cs
@@ -1,21 +1,156 @@
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace UndertaleModLib.Decompiler;
 
+/// <summary>
+/// Class that helps load game-specific data, which tailor some features (such as the decompiler) to specific games.
+/// </summary>
 public class GameSpecificResolver
 {
-    // Reads a built-in game-specific data file from the assembly
-    private static ReadOnlySpan<char> ReadGameSpecificDataFile(string filename)
+    private enum ConditionResult
     {
-        Assembly assembly = Assembly.GetExecutingAssembly();
-        string resourceName = $"UndertaleModLib.BuiltinGameSpecificData.{filename}";
+        Ignore,
+        Accept,
+        Reject
+    }
 
-        using Stream stream = assembly.GetManifestResourceStream(resourceName);
-        using StreamReader reader = new(stream);
-        return reader.ReadToEnd();
+    private static readonly Dictionary<string, Func<UndertaleData, string, ConditionResult>> _conditionEvaluators = new()
+    {
+        ["Always"] = (UndertaleData data, string value) => 
+        {
+            return ConditionResult.Accept;
+        },
+
+        ["DisplayName.Regex"] = (UndertaleData data, string value) =>
+        {
+            string displayName = data?.GeneralInfo?.DisplayName?.Content;
+            if (displayName is null)
+            {
+                return ConditionResult.Ignore;
+            }
+
+            Match m = Regex.Match(displayName, value, RegexOptions.CultureInvariant);
+            if (m.Success)
+            {
+                return ConditionResult.Accept;
+            }
+
+            return ConditionResult.Ignore;
+        },
+    };
+    private static readonly List<GameSpecificDefinition> _definitions = new();
+    private static bool _loadedDefinitions = false;
+
+    public class GameSpecificCondition
+    {
+        /// <summary>
+        /// Represents the kind of condition to be evaluated.
+        /// </summary>
+        public string ConditionKind { get; set; }
+
+        /// <summary>
+        /// Value to be used during evaluation of condition, if applicable.
+        /// </summary>
+        public string Value { get; set; }
+    }
+
+    public class GameSpecificDefinition
+    {
+        /// <summary>
+        /// Integer representing the order this definition should be evaluated/loaded in.
+        /// The lower this number is, the earlier this definition will be evaluated and loaded.
+        /// </summary>
+        /// <remarks>
+        /// Built-in definitions currently use values 0 (for GameMaker builtins) and 1 (for games).
+        /// </remarks>
+        public int LoadOrder { get; set; } = 100;
+
+        /// <summary>
+        /// List of conditions that will be evaluated sequentially, to match this game-specific definition.
+        /// </summary>
+        public List<GameSpecificCondition> Conditions { get; set; }
+
+        /// <summary>
+        /// Filename to be loaded as an Underanalyzer game-specific config, when this definition successfully matches.
+        /// If empty, null, or the file is otherwise nonexistent, this will be ignored.
+        /// </summary>
+        public string UnderanalyzerFilename { get; set; }
+
+        /// <summary>
+        /// Evaluates this game-specific definition against the given game data, returning whether this definition should be loaded.
+        /// </summary>
+        /// <returns>True if this definition should be loaded; false otherwise.</returns>
+        public bool Evaluate(UndertaleData data)
+        {
+            foreach (var condition in Conditions)
+            {
+                switch (_conditionEvaluators[condition.ConditionKind](data, condition.Value))
+                {
+                    case ConditionResult.Accept:
+                        return true;
+                    case ConditionResult.Reject:
+                        return false;
+                    case ConditionResult.Ignore:
+                        // Pass-through to next condition
+                        break;
+                }
+            }
+
+            // Default to reject
+            return false;
+        }
+
+        /// <summary>
+        /// Loads this game-specific definition.
+        /// </summary>
+        public void Load(UndertaleData data)
+        {
+            if (!string.IsNullOrEmpty(UnderanalyzerFilename))
+            {
+                string underanalyzerPath = Path.Combine("GameSpecificData", "Underanalyzer", UnderanalyzerFilename);
+                if (File.Exists(underanalyzerPath))
+                {
+                    data.GameSpecificRegistry.DeserializeFromJson(File.ReadAllText(underanalyzerPath));
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Forces a full reload of all game-specific definition files.
+    /// </summary>
+    public static void ReloadDefinitions()
+    {
+        // Mark definitions as loaded, and reset existing definitions
+        _loadedDefinitions = true;
+
+        // Scan directory for files, if it exists
+        string dir = Path.Combine("GameSpecificData", "Definitions");
+        if (Directory.Exists(dir))
+        {
+            foreach (string file in Directory.EnumerateFiles(dir, "*.json", SearchOption.TopDirectoryOnly))
+            {
+                _definitions.Add(JsonSerializer.Deserialize<GameSpecificDefinition>(File.ReadAllText(file)));
+            }
+        }
+
+        // Sort all definitions by their load order
+        _definitions.Sort((a, b) => a.LoadOrder);
+    }
+
+    /// <summary>
+    /// Loads game-specific definitions, if not loaded already. Call <see cref="ReloadDefinitions"/> to force a full reload.
+    /// </summary>
+    public static void LoadDefinitions()
+    {
+        if (!_loadedDefinitions)
+        {
+            ReloadDefinitions();
+        }
     }
 
     /// <summary>
@@ -23,20 +158,19 @@ public class GameSpecificResolver
     /// </summary>
     public static void Initialize(UndertaleData data)
     {
+        // Ensure all definitions are loaded
+        LoadDefinitions();
+
+        // Initialize empty game-specific registry for decompiler
         data.GameSpecificRegistry = new();
 
-        // TODO: make proper file/manifest for all games to use, not just UT/DR, and also not these specific names
-
-        // Read registry data files
-        string lowerName = data?.GeneralInfo?.DisplayName?.Content.ToLower(CultureInfo.InvariantCulture) ?? "";
-        data.GameSpecificRegistry.DeserializeFromJson(ReadGameSpecificDataFile("gamemaker.json"));
-        if (lowerName.StartsWith("undertale", StringComparison.InvariantCulture))
+        // Evaluate all definitions, and load all successful ones
+        foreach (var definition in _definitions)
         {
-            data.GameSpecificRegistry.DeserializeFromJson(ReadGameSpecificDataFile("undertale.json"));
-        }
-        if (lowerName == "survey_program" || lowerName.StartsWith("deltarune", StringComparison.InvariantCulture))
-        {
-            data.GameSpecificRegistry.DeserializeFromJson(ReadGameSpecificDataFile("deltarune.json"));
+            if (definition.Evaluate(data))
+            {
+                definition.Load(data);
+            }
         }
     }
 }
