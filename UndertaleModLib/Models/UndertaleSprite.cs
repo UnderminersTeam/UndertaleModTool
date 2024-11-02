2f9a2dfbefc725a3ee57e954bea932aefc92d1a6
luizzeroxis <eldorado.luiz@gmail.com>
2024-11-02 18:17:41 -0300

    Fix 2024.6 masks not showing sometimes

--- UndertaleModLib/Models/UndertaleSprite.cs
+++ UndertaleModLib/Models/UndertaleSprite.cs
@@ -279,9 +279,8 @@ public class UndertaleSprite : UndertaleNamedResource, PrePaddedObject, INotifyP
 
     public MaskEntry NewMaskEntry()
     {
-        MaskEntry newEntry = new MaskEntry();
         uint len = (Width + 7) / 8 * Height;
-        newEntry.Data = new byte[len];
+        MaskEntry newEntry = new MaskEntry(new byte[len], Width, Height);
         return newEntry;
     }
 
@@ -348,13 +347,19 @@ public class UndertaleSprite : UndertaleNamedResource, PrePaddedObject, INotifyP
     {
         public byte[] Data { get; set; }
 
+        // Not in the data file
+        public uint Width { get; set; }
+        public uint Height { get; set; }
+
         public MaskEntry()
         {
         }
 
-        public MaskEntry(byte[] data)
+        public MaskEntry(byte[] data, uint width, uint height)
         {
             this.Data = data;
+            this.Width = width;
+            this.Height = height;
         }
 
         /// <inheritdoc/>
@@ -853,7 +858,7 @@ public class UndertaleSprite : UndertaleNamedResource, PrePaddedObject, INotifyP
         uint total = 0;
         for (uint i = 0; i < maskCount; i++)
         {
-            newMasks.Add(new MaskEntry(reader.ReadBytes((int)len)));
+            newMasks.Add(new MaskEntry(reader.ReadBytes((int)len), width, height));
             total += len;
         }
 
