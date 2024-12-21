ed24966149a713d7aca0ee0dd39016c098f09b51
luizzeroxis <eldorado.luiz@gmail.com>
2024-12-20 21:19:50 -0300

    Uppercase hex

--- UndertaleModTool/Settings.cs
+++ UndertaleModTool/Settings.cs
@@ -51,8 +51,8 @@ namespace UndertaleModTool
         public double GlobalGridThickness { get; set; } = 1;
         public bool GridThicknessEnabled { get; set; } = false;
 
-        public string TransparencyGridColor1 { get; set; } = "#ff666666";
-        public string TransparencyGridColor2 { get; set; } = "#ff999999";
+        public string TransparencyGridColor1 { get; set; } = "#FF666666";
+        public string TransparencyGridColor2 { get; set; } = "#FF999999";
 
         public bool EnableDarkMode { get; set; } = false;
         public bool ShowDebuggerOption { get; set; } = false;
