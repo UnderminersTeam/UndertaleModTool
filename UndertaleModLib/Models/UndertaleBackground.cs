a3b50ef4ea5a4618d96687226f4d7f37993d4869
fixdude <88634667+fixdude@users.noreply.github.com>
2024-12-31 15:18:01 -0300

    Update UndertaleBackground.cs

--- UndertaleModLib/Models/UndertaleBackground.cs
+++ UndertaleModLib/Models/UndertaleBackground.cs
@@ -65,11 +65,10 @@ public class UndertaleBackground : UndertaleNamedResource, IDisposable
     /// </summary>
     public UndertaleTexturePageItem Texture { get; set; }
 
-
     /// <summary>
-    /// TODO: Functionality currently unknown. Game Maker Studio 2 only.
+    /// The offset of a tileset from the top left. Game Maker Studio 2 only.
     /// </summary>
-    public uint GMS2UnknownAlways2 { get; set; } = 2;
+    public uint GMS2TilesetOffset { get; set; } = 2;
 
     /// <summary>
     /// The width of a tile in this tileset. Game Maker Studio 2 only.
