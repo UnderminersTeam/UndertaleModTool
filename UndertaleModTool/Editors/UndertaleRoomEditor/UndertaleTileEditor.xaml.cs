e93ce956bd190fc00bd837cbe1310c75eaf57f1a
colinator27 <17358554+colinator27@users.noreply.github.com>
2025-01-20 15:11:54 -0500

    Remove platform warnings in GMS2 tile editor

--- UndertaleModTool/Editors/UndertaleRoomEditor/UndertaleTileEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleRoomEditor/UndertaleTileEditor.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma warning disable CA1416 // Validate platform compatibility
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -1087,3 +1089,5 @@ namespace UndertaleModTool
         #endregion
     }
 }
+
+#pragma warning restore CA1416 // Validate platform compatibility
