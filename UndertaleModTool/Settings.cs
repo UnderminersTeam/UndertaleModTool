09d8c687613a768c968fc85eee11aeeebaeee4ed
luizzeroxis <eldorado.luiz@gmail.com>
2025-03-31 22:53:20 -0300

    Remember window state when the program is closed

--- UndertaleModTool/Settings.cs
+++ UndertaleModTool/Settings.cs
@@ -66,6 +66,8 @@ namespace UndertaleModTool
         public DecompilerSettings DecompilerSettings { get; set; }
         public string InstanceIdPrefix { get; set; } = "inst_";
 
+        public WindowPlacementExtensions.WindowPlacement? MainWindowPlacement { get; set; } = null;
+
         public static Settings Instance { get; private set; }
 
         public static JsonSerializerOptions JsonOptions = new()
