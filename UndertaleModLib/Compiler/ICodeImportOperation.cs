9d364e6e8e08e870e1beefcd7ec38de192ffdbdc
colinator27 <17358554+colinator27@users.noreply.github.com>
2025-05-06 22:13:15 -0400

    Add trimmed-lines find&replace to CodeImportGroup

--- UndertaleModLib/Compiler/ICodeImportOperation.cs
+++ UndertaleModLib/Compiler/ICodeImportOperation.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 using UndertaleModLib.Models;
 
@@ -25,6 +27,7 @@ internal interface ICodeImportOperation
 /// </summary>
 internal readonly record struct CodeReplaceOperation(UndertaleCode CodeEntry, string Code) : ICodeImportOperation
 {
+    /// <inheritdoc/>
     public void Import(CodeImportGroup group)
     {
         group.CompileGroup.QueueCodeReplace(CodeEntry, Code);
@@ -36,6 +39,7 @@ internal readonly record struct CodeReplaceOperation(UndertaleCode CodeEntry, st
 /// </summary>
 internal readonly record struct CodeAppendOperation(UndertaleCode CodeEntry, string Code) : ICodeImportOperation
 {
+    /// <inheritdoc/>
     public void Import(CodeImportGroup group)
     {
         string finalCodeToCompile = string.Concat(group.DecompileExistingCode(CodeEntry), "\n", Code);
@@ -48,6 +52,7 @@ internal readonly record struct CodeAppendOperation(UndertaleCode CodeEntry, str
 /// </summary>
 internal readonly record struct CodePrependOperation(UndertaleCode CodeEntry, string Code) : ICodeImportOperation
 {
+    /// <inheritdoc/>
     public void Import(CodeImportGroup group)
     {
         string finalCodeToCompile = string.Concat(Code, "\n", group.DecompileExistingCode(CodeEntry));
@@ -59,8 +64,106 @@ internal readonly record struct CodePrependOperation(UndertaleCode CodeEntry, st
 /// Represents a code find and replace operation (by decompiling existing code, and performing find/replace on that string).
 /// </summary>
 internal readonly record struct CodeFindReplaceOperation
-    (UndertaleCode CodeEntry, string Search, string Replacement, bool IsRegex, bool CaseSensitive) : ICodeImportOperation
+    (UndertaleCode CodeEntry, string Search, string Replacement, bool IsRegex, bool CaseSensitive, bool TrimmedLines) : ICodeImportOperation
 {
+    private readonly record struct TrimmedLine(int WhitespaceStartIndex, int StartIndex, int EndIndex, int WhitespaceEndIndex);
+
+    /// <summary>
+    /// Returns a list of ranges, representing trimmed lines from the given string.
+    /// </summary>
+    private static List<TrimmedLine> SplitByTrimmedLines(ReadOnlySpan<char> str)
+    {
+        List<TrimmedLine> lines = new(16);
+        int lineWhitespaceStartIndex = 0;
+        int lineStartIndex = 0;
+        int lineEndIndex = 0;
+        bool beginningOfLine = true;
+        for (int i = 0; i < str.Length; i++)
+        {
+            // End of line
+            if (str[i] == '\n')
+            {
+                lines.Add(new(lineWhitespaceStartIndex, lineStartIndex, lineEndIndex, i));
+                lineWhitespaceStartIndex = i + 1;
+                lineStartIndex = i + 1;
+                lineEndIndex = i + 1;
+                beginningOfLine = true;
+                continue;
+            }
+            
+            // Find the beginning of a line
+            if (beginningOfLine && !char.IsWhiteSpace(str[i]))
+            {
+                lineStartIndex = i;
+                beginningOfLine = false;
+            }
+
+            // Find the end of a line
+            if (!beginningOfLine && !char.IsWhiteSpace(str[i]))
+            {
+                lineEndIndex = i + 1;
+            }
+        }
+        return lines;
+    }
+
+    /// <summary>
+    /// Replaces Search with Replacement on the given string, processing on a line-by-line basis, trimming each line's whitespace.
+    /// </summary>
+    private string ReplaceTrimmedLines(ReadOnlySpan<char> code, StringComparison comparison)
+    {
+        ReadOnlySpan<char> search = Search;
+        StringBuilder sb = new(code.Length);
+
+        // Split search and code into each line being matched
+        List<TrimmedLine> codeLines = SplitByTrimmedLines(code);
+        List<TrimmedLine> searchLines = SplitByTrimmedLines(search);
+
+        // Enumerate over code lines
+        for (int i = 0; i < codeLines.Count; i++)
+        {
+            // Check for match starting at this line
+            bool match = true;
+            if ((i + searchLines.Count) > codeLines.Count)
+            {
+                match = false;
+            }
+            else
+            {
+                // Compare all lines until any difference is found
+                for (int j = 0; j < searchLines.Count; j++)
+                {
+                    TrimmedLine codeLine = codeLines[i + j];
+                    TrimmedLine searchLine = searchLines[j];
+                    ReadOnlySpan<char> codeLineChars = code[codeLine.StartIndex..codeLine.EndIndex];
+                    ReadOnlySpan<char> searchLineChars = search[searchLine.StartIndex..searchLine.EndIndex];
+                    if (!codeLineChars.Equals(searchLineChars, comparison))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+            }
+
+            // If a match was found, insert replacement; otherwise, append current code line
+            if (match)
+            {
+                sb.Append(Replacement);
+                i += searchLines.Count;
+            }
+            else
+            {
+                TrimmedLine codeLine = codeLines[i];
+                ReadOnlySpan<char> fullCodeLineChars = code[codeLine.WhitespaceStartIndex..codeLine.WhitespaceEndIndex];
+                sb.Append(fullCodeLineChars);
+                sb.AppendLine();
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <inheritdoc/>
     public void Import(CodeImportGroup group)
     {
         // Decompile code
@@ -86,7 +189,14 @@ internal readonly record struct CodeFindReplaceOperation
             {
                 comparison = StringComparison.OrdinalIgnoreCase;
             }
-            finalCodeToCompile = decompilation.Replace(Search, Replacement, comparison);
+            if (TrimmedLines)
+            {
+                finalCodeToCompile = ReplaceTrimmedLines(decompilation, comparison);
+            }
+            else
+            {
+                finalCodeToCompile = decompilation.Replace(Search, Replacement, comparison);
+            }
         }
 
         // Queue lower level replace operation, if code has changed
