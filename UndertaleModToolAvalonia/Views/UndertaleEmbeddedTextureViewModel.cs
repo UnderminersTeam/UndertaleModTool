40072f09f88a82e73e46658f9472026bc5f8b603
luizzeroxis <eldorado.luiz@gmail.com>
2025-06-02 20:27:08 -0300

    Add save image button to embedded texture

--- UndertaleModToolAvalonia/Views/UndertaleEmbeddedTextureViewModel.cs
+++ UndertaleModToolAvalonia/Views/UndertaleEmbeddedTextureViewModel.cs
@@ -1,13 +1,57 @@
-﻿using UndertaleModLib.Models;
+﻿using Avalonia.Platform.Storage;
+using SkiaSharp;
+using System.IO;
+using System;
+using UndertaleModLib.Models;
+using UndertaleModToolAvalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace UndertaleModToolAvalonia.Views;
 
 public class UndertaleEmbeddedTextureViewModel
 {
+    public MainViewModel MainVM;
     public UndertaleEmbeddedTexture EmbeddedTexture { get; set; }
 
-    public UndertaleEmbeddedTextureViewModel(UndertaleEmbeddedTexture embeddedTexture)
+    public UndertaleEmbeddedTextureViewModel(UndertaleEmbeddedTexture embeddedTexture, IServiceProvider? serviceProvider = null)
     {
+        MainVM = (serviceProvider ?? App.Services).GetRequiredService<MainViewModel>();
+
         EmbeddedTexture = embeddedTexture;
+    }
+
+    public async void SaveImage()
+    {
+        IStorageFile? file = await MainVM.SaveFileDialog!(new FilePickerSaveOptions()
+        {
+            Title = "Save image",
+            FileTypeChoices = [
+               new FilePickerFileType("PNG files (.png)")
+                {
+                    Patterns = ["*.png"],
+                },
+                new FilePickerFileType("All files")
+                {
+                    Patterns = ["*"],
+                },
+            ],
+            DefaultExtension = ".png",
+        });
+
+        if (file is null)
+            return;
+
+        using Stream stream = await file.OpenWriteAsync();
+
+        var bitmap = new SKBitmap(EmbeddedTexture.TextureData.Image.Width, EmbeddedTexture.TextureData.Image.Height, SKColorType.Bgra8888, SKAlphaType.Unpremul);
+        var canvas = new SKCanvas(bitmap);
+
+        var op = new SKImageViewer.CustomDrawOperation();
+        op.SKImage = EmbeddedTexture.TextureData.Image;
+        op.RenderImage(canvas);
+
+        var result = bitmap.Encode(stream, SKEncodedImageFormat.Png, 100);
+        if (!result)
+            throw new InvalidOperationException();
     }
 }
