4ced0fd954ab8a0f7e8e179095f7284d5468a200
colinator27 <17358554+colinator27@users.noreply.github.com>
2026-01-06 16:43:41 -0500

    Some audio cleanup, make import errors warnings

--- UndertaleModTool/Editors/UndertaleEmbeddedAudioEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleEmbeddedAudioEditor.xaml.cs
@@ -2,21 +2,9 @@
 using NAudio.Vorbis;
 using NAudio.Wave;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using UndertaleModLib.Models;
 
 namespace UndertaleModTool
@@ -32,17 +20,23 @@ namespace UndertaleModTool
 
         private static readonly MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
 
-		public string FileType
+        public string FileType
         {
             get
             {
                 if (DataContext is not UndertaleEmbeddedAudio target)
+                {
                     return "Unknown";
-                
+                }
+
                 if (IsWav(target.Data))
+                {
                     return "WAV";
+                }
                 if (IsOgg(target.Data))
+                {
                     return "OGG";
+                }
                 return "Unknown";
             }
         }
@@ -59,23 +53,27 @@ namespace UndertaleModTool
 
         public void Unload(object sender, RoutedEventArgs e)
         {
-            if (waveOut != null)
-                waveOut.Stop();
+            waveOut?.Stop();
         }
 
         private void Import_Click(object sender, RoutedEventArgs e)
         {
             UndertaleEmbeddedAudio target = DataContext as UndertaleEmbeddedAudio;
 
-            OpenFileDialog dlg = new OpenFileDialog();
+            OpenFileDialog dlg = new();
 
-            if (IsWav(target.Data)) {
+            if (IsWav(target.Data)) 
+            {
                 dlg.DefaultExt = ".wav";
                 dlg.Filter = "WAV files|*.wav|All files|*";
-            } else if (IsOgg(target.Data)) {
+            } 
+            else if (IsOgg(target.Data)) 
+            {
                 dlg.DefaultExt = ".ogg";
                 dlg.Filter = "OGG files|*.ogg|All files|*";
-            } else {
+            } 
+            else 
+            {
                 dlg.DefaultExt = "";
                 dlg.Filter = "All files|*";
             }
@@ -85,13 +83,24 @@ namespace UndertaleModTool
                 try
                 {
                     byte[] data = File.ReadAllBytes(dlg.FileName);
-                    if (!IsWav(data) && !IsOgg(data)) {
-                        mainWindow.ShowError("Failed to import file!\r\nNot a WAV or OGG.", "Failed to import file");
+                    if (!IsWav(data) && !IsOgg(data)) 
+                    {
+                        if (mainWindow.ShowQuestionWithCancel("Warning: File being imported is not a WAV or OGG. Import anyway?\r\n\r\nThis may corrupt the sound.", MessageBoxImage.Warning, "Unknown format") != MessageBoxResult.Yes)
+                        {
+                            return;
+                        }
                     }
-                    else if ((IsWav(target.Data) && IsOgg(data)) || (IsOgg(target.Data) && IsWav(data))) {
-                        mainWindow.ShowError("Failed to import file!\r\nFormat doesn't match the original file.", "Failed to import file");
-                    } else
-                        target.Data = data;
+                    else if ((IsWav(target.Data) && IsOgg(data)) || (IsOgg(target.Data) && IsWav(data)))
+                    {
+                        if (mainWindow.ShowQuestionWithCancel(
+                            "Warning: Filetype being imported does not match existing filetype. Import anyway?\r\n\r\n" +
+                            "This may corrupt the sound, unless sound asset compression settings are adjusted as well.", MessageBoxImage.Warning, "Format mismatch") != MessageBoxResult.Yes)
+                        {
+                            return;
+                        }
+                    } 
+                    target.Data = data;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileType)));
                 }
                 catch (Exception ex)
                 {
@@ -106,13 +115,18 @@ namespace UndertaleModTool
 
             SaveFileDialog dlg = new SaveFileDialog();
 
-            if (IsWav(target.Data)) {
+            if (IsWav(target.Data)) 
+            {
                 dlg.DefaultExt = ".wav";
                 dlg.Filter = "WAV files|*.wav|All files|*";
-            } else if (IsOgg(target.Data)) {
+            } 
+            else if (IsOgg(target.Data)) 
+            {
                 dlg.DefaultExt = ".ogg";
                 dlg.Filter = "OGG files|*.ogg|All files|*";
-            } else {
+            } 
+            else 
+            {
                 dlg.DefaultExt = "";
                 dlg.Filter = "All files|*";
             }
@@ -133,55 +147,61 @@ namespace UndertaleModTool
         private void InitAudio()
         {
             if (waveOut == null)
+            {
                 waveOut = new WaveOutEvent() { DeviceNumber = 0 };
+            }
             else if (waveOut.PlaybackState != PlaybackState.Stopped)
+            {
                 waveOut.Stop();
+            }
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
             UndertaleEmbeddedAudio target = DataContext as UndertaleEmbeddedAudio;
 
-            if (target.Data.Length > 4)
+            try
             {
-                try
+                if (IsWav(target.Data))
                 {
-                    if (IsWav(target.Data))
-                    {
-                        wavReader = new WaveFileReader(new MemoryStream(target.Data));
-                        InitAudio();
-                        waveOut.Init(wavReader);
-                        waveOut.Play();
-                    }
-                    else if (IsOgg(target.Data))
-                    {
-                        oggReader = new VorbisWaveReader(new MemoryStream(target.Data));
-                        InitAudio();
-                        waveOut.Init(oggReader);
-                        waveOut.Play();
-                    } else
-                        mainWindow.ShowError("Failed to play audio!\r\nNot a WAV or OGG.", "Audio failure");
-                } catch (Exception ex)
-                {
-                    waveOut = null;
-                    mainWindow.ShowError("Failed to play audio!\r\n" + ex.Message, "Audio failure");
+                    wavReader = new WaveFileReader(new MemoryStream(target.Data));
+                    InitAudio();
+                    waveOut.Init(wavReader);
+                    waveOut.Play();
                 }
+                else if (IsOgg(target.Data))
+                {
+                    oggReader = new VorbisWaveReader(new MemoryStream(target.Data));
+                    InitAudio();
+                    waveOut.Init(oggReader);
+                    waveOut.Play();
+                }
+                else
+                {
+                    mainWindow.ShowError("Failed to play audio!\r\nNot a WAV or OGG.", "Audio failure");
+                }
+            } 
+            catch (Exception ex)
+            {
+                waveOut = null;
+                mainWindow.ShowError("Failed to play audio!\r\n" + ex.Message, "Audio failure");
             }
         }
 
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
-            if (waveOut != null)
-                waveOut.Stop();
+            waveOut?.Stop();
         }
-		
-		private bool IsWav(byte[] data) {
-			return data[0] == 'R' && data[1] == 'I' && data[2] == 'F' && data[3] == 'F';
-		}
-		
-		private bool IsOgg(byte[] data) {
-            return data[0] == 'O' && data[1] == 'g' && data[2] == 'g' && data[3] == 'S';
-		}
+        
+        private static bool IsWav(byte[] data) 
+        {
+            return data.Length >= 4 && data[0] == 'R' && data[1] == 'I' && data[2] == 'F' && data[3] == 'F';
+        }
+
+        private static bool IsOgg(byte[] data) 
+        {
+            return data.Length >= 4 && data[0] == 'O' && data[1] == 'g' && data[2] == 'g' && data[3] == 'S';
+        }
     }
 }
